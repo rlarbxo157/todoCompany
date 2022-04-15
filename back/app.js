0684f85e4511d0c4cecfea32e1d556166675b710
20140108 <rlarbxo157@naver.com>
2022-04-15 17:28:12 +0900

    api 설계

--- back/app.js
+++ back/app.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const app = express();
-var cors = require('cors');
 const postRouter = require('./routes/post');
+var cors = require('cors');
 
 const corsOptions = {
     origin:'http://localhost:3000'
 }
-
-// app.get('/',(req,res)=> {
-//     res.header("Access-Control-Allow-Origin","http://localhost:3000");
-//     res.json({
-//        id:1,
-//        name:'김규태'
-//     })
-// });
 
 app.get('/',(req,res)=> {
     res.header("Access-Control-Allow-Origin","http://localhost:3000");
