dbacddc299a9b1fdd244597291da6d0d4949b58d
20140108 <rlarbxo157@naver.com>
2022-04-15 16:04:58 +0900

    front setting -> create-next-app --typescript 사용하여 next.js 에 tyepscript 연동

--- back/app.js
+++ back/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const postRouter = require('./routes/post');
 
 app.get('/',(req,res)=> {
-   res.send('hello express');
+   res.json({
+       id:1,
+       name:'김규태'
+   })
 });
 
+app.use('/post',postRouter);
 
 app.listen(8000,()=> {
     console.log('server running');
